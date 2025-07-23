9be4ddfb04ea180b6c95d9a010ceea37efb10673
Ammar <ammarzafar028@gmail.com>
2025-07-24 02:22:21 +0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'End-to-End-Medical-Chatbot-Generative-AI',
+    packages = find_packages(),
+    version = '0.0.0',
+    author_email = "Ammarzafar028@gmail.com",
+    author = 'Ammar Zafar',
+    license = 'MIT',
+    install_requires = ["setuptools"]
+)
